5fe035303d34fcb12fdb7804278d637f857a75b0
Edison <solisedison@outlook.com>
2023-12-10 09:41:27 -0500

    fixed the routes

---
+++ Users/url.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from knox import views as knox_views
+urlpatterns=[
+    path('login',views.login_api),#Hay que enviar los datos por el emtodo POST para logearse en
+    path('user',views.get_user_data),#Busca un usuario por
+    path('register',views.register_api),#envia un metodo POST par crear un usuario en raw 
+    path('logout/',knox_views.LogoutView.as_view()),
+    path('logoutall',knox_views.LogoutAllView.as_view())
+]
